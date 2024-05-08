0b3ed8b7911810c927c710177fd5cb771bf80831
mdhabibi <habibi.physics@gmai.com>
2024-05-08 10:42:13 +0200

    Updated Docstrings for more clarification

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,18 +1,67 @@
 import os
-from PIL import Image
 import pandas as pd
 
 
 def load_dataset_labels(main_folder_path):
-    """Load dataset labels based on folder names."""
+    """
+    Retrieves a list of directory names within a specified parent directory,
+    treating each directory name as a dataset label.
+
+    Parameters:
+    - main_folder_path (str): The file path to the directory containing
+      the dataset's subdirectories.
+
+    Returns:
+    - list: A list of strings where each string is a label derived from
+      the names of subdirectories within the specified main directory.
+
+    Example:
+    Assuming a directory structure as follows:
+        - /path/to/data/
+            - basophil/
+            - eosinophil/
+    Calling `load_dataset_labels('/path/to/data/')` would return
+    `['basophil', 'eosinophil']`.
+    """
     labels = [os.path.basename(f.path) for f in
               os.scandir(main_folder_path) if f.is_dir()]
     return labels
 
 
 def load_images_with_labels(main_folder_path):
-    """Load images from each subfolder and count them,
-       returning both the images and a summary DataFrame."""
+    """
+    Load and count images from each labeled subfolder within
+    a specified directory, organizing the results into
+    a dictionary and a pandas DataFrame.
+
+    Parameters:
+    - main_folder_path (str): The file path to the directory containing
+      labeled subfolders.
+
+    Returns:
+    - tuple: A tuple containing two elements:
+        1. A dictionary with each key being a label and the value being
+           a list of image file names.
+        2. A pandas DataFrame summarizing the number of images per label.
+
+    Example:
+    Assuming a directory structure as follows:
+        - /path/to/data/
+            - basophil/
+                - basophil1.jpg
+                - basophil2.jpg
+            - eosinophil/
+                - eosinophil1.jpg
+    Calling `load_images_with_labels('/path/to/data/')` would return
+    a dictionary and a DataFrame:
+    Dictionary:
+        {'basophil': ['basophil1.jpg', 'basophil2.jpg'],
+         'eosinophil': ['eosinophil1.jpg']}
+    DataFrame:
+        Label      | Number of Images
+        basophil   | 2
+        eosinophil | 1
+    """
     images_with_labels = {}  # {label: images list}
 
     # Gather images from each label's folder
@@ -29,7 +78,6 @@ def load_images_with_labels(main_folder_path):
     for label, images in images_with_labels.items():
         label_list.append(label)
         num_images.append(len(images))
-        # print(f"Label: {label}, ---> Number of Images: {len(images)}")
 
     # Convert lists to DataFrame
     data = pd.DataFrame({
